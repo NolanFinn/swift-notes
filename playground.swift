5e07f1c8f1c949be560e4f61ab3b53efb6b6f74e
NolanFinn <NolanFinn@gmail.com>
2016-01-25 20:36:52 -0600

    Update playground.swift

--- playground.swift
+++ playground.swift
@@ -1,21 +1,16 @@
-
-
-var participants = ["JWK", "NBF"]
-
-var numberOfParticpants = participants.count
-
 struct menuItem {
-    var itemUnits: Double = 1
+    var itemUnits: Double = 2
     var itemName: String = ""
     var itemPrice: Double = 0.00
+    var itemPurchasers: [String] = []
     var itemPricePerPerson: Double {
         get {
-            return (itemUnits*itemPrice)/Double(participants.count)
+            return (itemUnits*itemPrice)/Double(itemPurchasers.count)
         }
     }
 }
 
-let item1 = menuItem(itemUnits: 2, itemName: "The Feature", itemPrice: 8)
+let item1 = menuItem(itemUnits: 2, itemName: "The Feature", itemPrice: 24, itemPurchasers: ["JWK","RK"])
 
 let formatter = NSNumberFormatter()
 formatter.numberStyle = .CurrencyStyle
